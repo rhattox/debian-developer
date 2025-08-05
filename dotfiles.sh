5cc7ffb7eaff2e5483545d46720039f9f9109db9
Bruno Souto <65253099+rhattox@users.noreply.github.com>
2025-08-05 16:16:47 -0300

    Update dotfiles.sh

--- dotfiles.sh
+++ dotfiles.sh
@@ -10,11 +10,14 @@ cp -a ./dotfiles/.tmux.conf ${HOME_USER}/.tmux.conf
 chown ${USER}:root ${HOME_USER}/.tmux.conf
 chmod 0750 ${HOME_USER}/.tmux.conf
 
+mkdir -p ${HOME_USER}/.config/i3
+
 cp -a ./dotfiles/i3conf ${HOME_USER}/.config/i3/config
 chown ${USER}:root ${HOME_USER}/.config/i3/config
 chmod 0750 ${HOME_USER}/.config/i3/config
 
-cp -a ./dotfiles/i3conf ${HOME_USER}/.config/i3status/config
-chown ${USER}:root ${HOME_USER}/.config/i3status/config
-chmod 0750 ${HOME_USER}/.config/i3status/config
+mkdir -p ${HOME_USER}/.config/i3status
 
+cp -a ./dotfiles/i3statusconf ${HOME_USER}/.config/i3status/config
+chown -R ${USER}:root ${HOME_USER}/.config/i3status/config
+chmod -R 0750 ${HOME_USER}/.config/i3status/config
