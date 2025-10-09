30c77a0ba34cc2e3d4c7e5d9cdc15071eca8bbec
Bruno Souto <65253099+rhattox@users.noreply.github.com>
2025-10-08 21:21:53 -0300

    add lightdm and xorg

--- apt-packages.sh
+++ apt-packages.sh
@@ -12,6 +12,6 @@ apt install -y curl wget net-tools iputils-ping dnsutils telnet traceroute nmap 
 apt install -y sudo git vim build-essential make cmake gcc tree xclip zip unzip rsync openssl
 apt install -y libbz2-dev libev-dev libffi-dev libgdbm-dev liblzma-dev libncurses-dev libreadline-dev libsqlite3-dev libssl-dev tk-dev zlib1g-dev
 apt install -y htop iftop iotop sysstat glances dstat
-apt install -y dmenu feh i3 i3status i3lock xrdp gnome-terminal
+apt install -y dmenu feh lightdm xserver-xorg-input-all i3 i3status i3lock xrdp gnome-terminal 
 apt install -y jq yq fzf ca-certificates apt-transport-https gnupg lsb-release
 apt install -y fonts-liberation fonts-liberation2
