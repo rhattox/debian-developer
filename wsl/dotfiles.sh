a06bfc6d3519ae2cbc5be19950c46e890fb0f9c9
rhattox <120387348+rhattox@users.noreply.github.com>
2026-08-17 21:21:17 -0300

    update config

--- wsl/dotfiles.sh
+++ wsl/dotfiles.sh
@@ -6,12 +6,19 @@ echo "#### INSTALL .DOTFILES ####"
 echo "###########################"
 echo "###########################"
 
-cp -a ${EXECUTION_PATH}/dotfiles/.gitconfig ${HOME_USER}/.gitconfig
+# TODO
+#	add a env var for the common/dotfiles
+#
+
+cp -a ${EXECUTION_PATH}/../common/dotfiles/.gitconfig ${HOME_USER}/.gitconfig
 chown ${USER}:${USER} ${HOME_USER}/.gitconfig
 chmod 0750 ${HOME_USER}/.gitconfig
 
-cp -a ${EXECUTION_PATH}/dotfiles/.tmux.conf ${HOME_USER}/.tmux.conf
+cp -a ${EXECUTION_PATH}/../common/dotfiles/.tmux.conf ${HOME_USER}/.tmux.conf
 chown ${USER}:${USER} ${HOME_USER}/.tmux.conf
 chmod 0750 ${HOME_USER}/.tmux.conf
 
+# TODO 
+#	add a checker here for github binary
+#
 git clone ${DOTFILES_REPOSITORY} ${HOME_USER}/.config/nvim
